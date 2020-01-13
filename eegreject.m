00fd3c360e5b711d47478b0355e6556ac5c3923c
Colin Quirk <cquirk@uchicago.edu>
2020-01-13 16:44:58 -0600

    complete reref, start eye data, add help comments

--- eegreject.m
+++ eegreject.m
@@ -6,16 +6,17 @@ eeglab
 
 %% Options
 
-eeglabFolder = '/Applications/eeglab2019_1/';
-
 subjectParentDir = 'test_data';
-subjectDirectories = {'6'};
+subjectDirectories = {'7'};  % optionally {} for recursive search
 
 lowboundFilterHz = 0.01;
 highboundFilterHz = 30;
 
-rerefChanLabels = {'TP9', 'TP10'};
+rerefType = 'mastoid'; % 'none', 'average', or 'mastoid' 
 rerefExcludeChans = {'HEOG', 'VEOG', 'StimTrak'};
+customEquationList = '';  % optional
+
+EYEEEGKeyword = 'SYNC';
 
 %% Setup 
 
@@ -36,49 +37,83 @@ end
 
 
 log = fopen('log.txt', 'a+t');
-fprintf(log, ['Run started: ', datestr(now), '\n']);
-
-chanLocsFile = fullfile(eeglabFolder, 'plugins/dipfit/standard_BESA/standard-10-5-cap385.elp');
+fprintf(log, ['Run started: ', datestr(now), '\n\n']);
 %% Main loop
 
 for subdir=1:numel(subjectDirectories)
     subdirpath = fullfile(subjectParentDir, subjectDirectories{subdir});
     
     disp(['Running ', subdirpath])
-    fprintf(log, ['Running ', subdirpath, '\n']);
+    fprintf(log, ['Running ', subdirpath, '\n\n']);
     
-    vhdr_dir = dir(fullfile(subdirpath, '*.vhdr'));
+    vhdrDir = dir(fullfile(subdirpath, '*.vhdr'));
     
-    if numel(vhdr_dir) == 0
+    if numel(vhdrDir) == 0
         warning(['Skipping ', subdirpath, '. No vhdr file found.'])
+        fprintf(log, ['Skipping ', subdirpath, '. No vhdr file found.\n\n']);
         continue
-    elseif numel(vhdr_dir) > 1
+    elseif numel(vhdrDir) > 1
         warning(['Skipping ', subdirpath, '. More than one vhdr file found.'])
+        fprintf(log, ['Skipping ', subdirpath, '. More than one vhdr file found.\n\n']);
         continue
     end
     
-    vhdr_filename = vhdr_dir(1).name;
-
-    EEG = pop_loadbv(subdirpath, vhdr_filename);
+    vhdrFilename = vhdrDir(1).name;
     
-    EEG.setname = vhdr_filename(1:end-5);
+    ascDir = dir(fullfile(subdirpath, '*.asc'));
+    
+    if numel(ascDir) == 0
+        warning(['Skipping ', subdirpath, '. No asc file found.'])
+        fprintf(log, ['Skipping ', subdirpath, '. No vhdr file found.\n\n']);
+        continue
+    elseif numel(ascDir) > 1
+        warning(['Skipping ', subdirpath, '. More than one asc file found.'])
+        fprintf(log, ['Skipping ', subdirpath, '. More than one asc file found.\n\n']);
+        continue
+    end
+    
+    ascFullFilename = fullfile(subdirpath, ascDir(1).name);
+
+    EEG = pop_loadbv(subdirpath, vhdrFilename);
+    
+    EEG.setname = vhdrFilename(1:end-5);
     
     if lowboundFilterHz ~= 0 && highboundFilterHz ~= 0
+        fprintf(log, sprintf('Bandpass filtering with lowboundFilterHz = %f and highboundFilterHz=%f\n\n', lowboundFilterHz, highboundFilterHz));
         EEG = pop_basicfilter(EEG, 1:EEG.nbchan, 'Boundary', 'boundary', 'Cutoff', [lowboundFilterHz highboundFilterHz], 'Design', 'butter', 'Filter', 'bandpass', 'Order', 2);
     elseif highboundFilterHz ~= 0
+        fprintf(log, sprintf('Lowpass filtering with highboundFilterHz=%f\n\n', highboundFilterHz));
         EEG = pop_basicfilter(EEG, 1:EEG.nbchan, 'Boundary', 'boundary', 'Cutoff', highboundFilterHz, 'Design', 'butter', 'Filter', 'lowpass', 'Order', 2);
     elseif lowboundFilterHz ~= 0
+        fprintf(log, sprintf('Highpass filtering with lowboundFilterHz = %f\n\n', lowboundFilterHz));
         EEG = pop_basicfilter(EEG, 1:EEG.nbchan, 'Boundary', 'boundary', 'Cutoff', lowboundFilterHz, 'Design', 'butter', 'Filter', 'highpass', 'Order', 2);
     end
     
-    rerefChanIndexes = find(ismember({EEG.chanlocs(:).labels}, rerefExcludeChans));
-    
-    %EEG = pop_chanedit(EEG, 'append', 10, 'changefield', {11 'labels' 'TP10'}, 'lookup', chanLocsFile, 'rplurchanloc', 1);
-    %EEG = pop_reref(EEG, [], 'exclude', rerefChanIndexes, 'keepref', 'on');  % temporarily reref to average
-    
-    if ~isempty(rerefChanLabels)
-        %EEG = pop_reref(EEG, rerefChanLabels, 'exclude', rerefChanIndexes);
+    if ~strcmp(rerefType, 'none')
+        if ~strcmp(customEquationList, '')
+            equationList = customEquationList;
+        else
+            equationList = get_chan_equations(EEG, rerefType, rerefExcludeChans);
+        end
+        
+        fprintf(log, 'Rereferencing with following equation list:\n');
+        fprintf(log, strjoin(equationList, '\n'));
+        fprintf(log, '\n\n');
+        
+        EEG = pop_eegchanoperator( EEG, equationList);
+    else
+        fprintf(log, 'Skipping rereferencing because rerefType = "none"\n\n');
     end
+    
+    EYEEEGMatFilename = [ascFullFilename(1:end-4) '_eye.mat'];
+    
+    fprintf(log, sprintf('Parsing asc file: %s\n\n', ascFullFilename));
+    parseeyelink(ascFullFilename, EYEEEGMatFilename, EYEEEGKeyword);
+
+
+    %EEG = pop_importeyetracker(EEG,[filename(1:end-5) '_eye.mat'],[1 2] ,[2 3] ,{'GAZE_X' 'GAZE_Y'},0,1,0,0);
+    EEG = pop_importeyetracker(EEG, EYEEEGMatFilename, [1 2], [2 3 5 6], {'L_GAZE_X' 'L_GAZE_Y' 'R_GAZE_X' 'R_GAZE_Y'}, 0, 1, 0, 0);
+
 
 
 % pull in eye data and sync
@@ -105,3 +140,30 @@ fclose(log);
 
 %% Helper Functions
 
+function equationList = get_chan_equations(EEG, rerefType, excludes)
+    if ~any(strcmp({'mastoid', 'average'}, rerefType))
+        error('rerefType must be "mastoid" or "average"')
+    end
+
+    baseEquation = 'ch%d = ch%d - (%s) Label %s';
+    
+    allLocs = {EEG.chanlocs.labels};
+    
+    includedChanLabels = allLocs;
+    includedChanLabels(ismember(allLocs, excludes)) = [];
+    [~, includedChanIndexes] = ismember(includedChanLabels, allLocs);
+    
+    equationList = {};
+  
+    if strcmp(rerefType, 'average')
+        equationString = sprintf('avgchan(%s)', mat2colon(includedChanIndexes));
+    else
+        refIdx = find(strcmp({EEG.chanlocs.labels}, 'TP9'));
+        equationString = sprintf('.5 * ch%d', refIdx);
+    end
+    
+    for i=includedChanIndexes
+        equationList{end + 1} = sprintf(baseEquation, i, i, equationString, allLocs{i}); %#ok<AGROW>
+    end
+    
+end
